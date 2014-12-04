7e584e5e57c271aa057a0e93a6ecca3052764816
misogi <dev@misogi.info>
2014-12-04 18:44:09 +0900

    fix security issue

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-error_reporting(-1);
-
 $autoloader = require(__DIR__ . '/../vendor/autoload.php');
 $autoloader->add('BetterErrorTests\\', __DIR__ . '/../src/BetterErrorTests');
 \BetterError\BetterError::register();
